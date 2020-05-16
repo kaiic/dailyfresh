d6b871ed4491d66ea9d5e5ed734d46bc6499c2ab
kaiic <1606155072@qq.com>
2020-05-16 18:36:25 +0800

    创建视图login

--- view.py
+++ view.py
@@ -1,7 +1,8 @@
 from django import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
 	return HttpResponse('index')
 
-
+def login(request):
+	return HttpResponse('/index')
